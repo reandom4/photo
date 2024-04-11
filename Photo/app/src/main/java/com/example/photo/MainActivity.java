58b2e6145edc9e2bc4c253f0650ac6c061fc6dcb
reandom4 <vovic200966@gmail.com>
2024-04-11 22:58:42 +0300

    123

---
+++ Photo/app/src/main/java/com/example/photo/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.photo;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private ImageView imageView;
+
+    private Bitmap imageBitmap;
+
+    ActivityResultLauncher<Intent> launcher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult o) {
+                    int resulCode = o.getResultCode();
+                    Intent data = o.getData();
+                    if (resulCode == RESULT_OK)
+                    {
+                        Bitmap photo = (Bitmap) data.getExtras().get("data");
+                        imageView.setImageBitmap(photo);
+                    }
+                }
+            }
+    );
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                launcher.launch(intent);
+            }
+        });
+    }
+}
